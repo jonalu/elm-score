ad20c3189602d4da9b25289cdf859a7b55b2d513
Jonas Lundeland <jlundela@gmail.com>
2017-03-09 13:54:33 +0100

    REST api urls use https protocol

--- src/Urls.elm
+++ src/Urls.elm
@@ -2,9 +2,9 @@ module Urls exposing (..)
 
 
 premierLeagueCurrentRound =
-    "http://rest.tv2.no/sports-dw-rest/sport/schedule/tournament/230/season/338?currentRound=true"
+    "https://rest.tv2.no/sports-dw-rest/sport/schedule/tournament/230/season/338?currentRound=true"
 
 
 match : Int -> String
 match id =
-    "http://rest.tv2.no/sports-dw-rest/sport/football/match/" ++ toString id
+    "https://rest.tv2.no/sports-dw-rest/sport/football/match/" ++ toString id
